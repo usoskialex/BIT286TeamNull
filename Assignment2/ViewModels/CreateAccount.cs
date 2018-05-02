c4b8ea9947cbff77316f41e24dba2a1d641ae729
Emilio <Emilio@VALIANCE>
2018-05-01 20:07:19 -0700

    Almost done with the Create account

---
+++ Assignment2/ViewModels/CreateAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Assignment2.ViewModels
+{
+    public class CreateAccount
+    {
+        [DisplayName("First Name:")]
+        [Required]
+        public string FirstName { get; set; }
+
+        [DisplayName("Last Name:")]
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Pal's Name")]
+        [DataType(DataType.Password)]
+        private string Password { get; set; }
+    }
+}
